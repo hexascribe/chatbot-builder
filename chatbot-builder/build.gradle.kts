b956da9a144cd04c89f3856ddd48abd76469b020
xyzwilliamxyz <wiltang256@gmail.com>
2023-06-21 16:31:47 -0300

    Remove java config from gradle.properties and set JDK 17

--- chatbot-builder/build.gradle.kts
+++ chatbot-builder/build.gradle.kts
@@ -77,11 +77,8 @@ android {
         minSdk = libs.versions.min.sdk.get().toInt()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlin {
-        jvmToolchain(11)
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
--- samples/android/build.gradle.kts
+++ samples/android/build.gradle.kts
@@ -31,10 +31,7 @@ android {
         versionName = "1.0"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlin {
-        jvmToolchain(11)
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
